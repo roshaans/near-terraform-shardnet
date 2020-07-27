c73d6cc0c08aa636ccd1feba4bb21e184e8236be
The Passive Trust <thepassivetrust@Wades-MBP-2.hitronhub.home>
2020-07-26 22:14:20 -0700

    Remove isolated machine in private subnet from installation

--- near/modules/validator/main.tf
+++ near/modules/validator/main.tf
@@ -5,11 +5,12 @@ module "ami" {
 resource "aws_instance" "near_validator" {
   
 
-  ami                    = module.ami.ami_ids.ubuntu_18_04
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name               = var.key_pair_name
+  ami                         = module.ami.ami_ids.ubuntu_18_04
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = var.key_pair_name
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size = var.volume_size
@@ -19,9 +20,26 @@ resource "aws_instance" "near_validator" {
     file("${path.module}/../startup-scripts/install-base.sh"),
     file("${path.module}/../startup-scripts/install-docker.sh"),
     file("${path.module}/../startup-scripts/install-chrony.sh"),
-   # templatefile("${path.module}/../startup-scripts/run-validator-node.sh", {  //Add startup script
-   #
-   # }),
+    templatefile("${path.module}/../startup-scripts/install-nearup.sh", {     //Set up with running script
+    validator_key  = var.validator.validator_key
+    node_key       = var.validator.node_key
+    account_id     = var.validator.account_id
+    stakingpool_id = var.validator.stakingpool_id
+    network        = var.network
+    }),
+    templatefile("${path.module}/../startup-scripts/install-monitoring.sh", {     //Set up with running script
+    email_address  = var.validator.gmail_address
+    email_password = var.validator.gmail_password
+    stakingpool_id = var.validator.stakingpool_id
+    }),
+    templatefile("${path.module}/../startup-scripts/install-warchest_bot.sh", {     
+    stakepool_id            = var.validator.stakingpool_id
+    account_id              = var.validator.account_id
+    network                 = var.network
+    seat_price_percentage   = var.validator.seat_price_percentage
+    lower_bid_threshold     = var.validator.lower_bid_threshold
+    upper_bid_threshold     = var.validator.upper_bid_threshold
+     }),
     file("${path.module}/../startup-scripts/final-hardening.sh")
   ])
 
