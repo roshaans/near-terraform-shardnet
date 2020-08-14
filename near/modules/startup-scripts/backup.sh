43be442713dc86f55b3965c43571fad2363237d2
Wade Abel <thepassivetrust@gmail.com>
2020-08-13 17:59:56 -0700

    Add example secret

---
+++ near/modules/startup-scripts/backup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# tar -zcvf -----make tarball
+
+#install aws
+
+# in ---------- tar cvfz - /var/test | aws s3 cp - s3://tests/test1.tar.gz
+
+# out -------------- aws s3 cp s3://example-bucket/file.tar.gz - | tar -xz
--- near/modules/validator/outputs.tf
+++ near/modules/validator/outputs.tf
@@ -1,3 +1,11 @@
 output instances {
   value = aws_instance.near_validator
 }
+
+output public_ip {
+  value = aws_instance.near_validator.public_ip
+}
+
+output private_ip {
+  value = aws_instance.near_validator.private_ip
+}
